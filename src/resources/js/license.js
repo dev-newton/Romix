21643bb4712107db48ecb6f173593c03ea93ba7f
Imonjirie Newton <newtonimonjirie@gmail.com>
2020-10-10 22:09:20 +0100

    romix demo v1

---
+++ src/resources/js/license.js
@@ -0,0 +1,14 @@
+/*
+
+Theme:  Romicxpresslogistics
+Version: 1.1.0
+Product Page: https://webpixels.io/themes/quick-website-ui-kit
+License: MIT
+Author: Webpixels
+Author URI: https://webpixels.io
+
+---
+
+Copyright 2020 Webpixels
+
+*/
